1c370db3e56414698986153abfaf2fe9567065ca
syncretismn <113224346+syncretismn@users.noreply.github.com>
2024-12-09 12:59:39 +0000

    1

--- app.py
+++ app.py
@@ -3,6 +3,12 @@
 from flask import Flask #网页创建包
 from flask import render_template,request
 import textblob
+import google.generativeai as genai
+import os
+api=os.getenv("makersuite")
+#api='AIzaSyC3PgQn6g619clqC5CZd_CymqrGq7O2iC0'
+genai.configure(api_key=api)
+model = genai.GenerativeModel("gemini-1.5-flash")
 
 app = Flask("__name__")#
 
@@ -25,5 +31,15 @@ def SA_result():
     r=textblob.TextBlob(q).sentiment
     return(render_template("SA_result.html",r=r))
 
+@app.route("/genAI",methods=["GET","POST"])
+def GenAI():
+    return(render_template("genAI.html"))
+
+@app.route("/genAI_result",methods=["GET","POST"])
+def genAI_result():
+    q =request.form.get("q")
+    r=model.generate_content(q)
+    r=r.candidates[0].content.parts[0].text
+    return(render_template("genAI_result.html",r=r))
 if __name__ == "__main__": ## __is for system to use
     app.run()
